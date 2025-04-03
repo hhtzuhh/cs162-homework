d58a26ee30a5714a9fddffd33fa795c039743f4e
tzuhan <hhtzuhh@gmail.com>
2025-04-03 10:51:34 +0000

    thread for word count

--- hw-list/pwords.c
+++ hw-list/pwords.c
@@ -31,6 +31,25 @@
 
 #include "word_count.h"
 #include "word_helpers.h"
+// struct to hold arguments for each thread
+typedef struct {
+  char* filename;
+  word_count_list_t* wclist;
+} threads_args_t;
+
+// thread function that processes a file
+void* process_file(void* args) {
+  threads_args_t* data = (threads_args_t*) args;
+  FILE* file = fopen(data->filename, "r");
+  if (file == NULL) {
+    printf("ERROR; could not open file %s\n", data->filename);
+    exit(-1);
+  }
+  count_words(data->wclist, file);
+  fclose(file);
+  pthread_exit(NULL);//what is this?
+}
+
 
 /*
  * main - handle command line, spawning one thread per file.
@@ -45,6 +64,30 @@ int main(int argc, char* argv[]) {
     count_words(&word_counts, stdin);
   } else {
     /* TODO */
+    int num_threads = argc - 1;
+    // array of threads to be created
+    //Needed when num_threads is not known at compile time. 
+    //otherwise, use pthread_t threads[num_threads]; , C99 seems to support this, at compile time
+    pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
+    threads_args_t *threads_data = malloc(num_threads * sizeof(threads_args_t));
+    // create a thread for each file
+    for (int i = 0; i < num_threads; i++) {
+      threads_data[i].filename = argv[i + 1];
+      threads_data[i].wclist = &word_counts;
+
+      int rc = pthread_create(&threads[i], NULL, process_file, &threads_data[i]);
+      if (rc) {
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+      }
+    }
+    // wait for all threads to finish
+    for (int i = 0; i < num_threads; i++) {
+      pthread_join(threads[i], NULL);
+    }
+    //clean up
+    free(threads);
+    free(threads_data);
   }
 
   /* Output final result of all threads' work. */
--- hw-list/word_count_p.c
+++ hw-list/word_count_p.c
@@ -30,30 +30,102 @@
 
 #include "word_count.h"
 
-void init_words(word_count_list_t* wclist) { /* TODO */
+void init_words(word_count_list_t* wclist) { 
+  /* TODO */
+  list_init(&wclist->lst);
+  pthread_mutex_init(&wclist->lock, NULL);
 }
 
 size_t len_words(word_count_list_t* wclist) {
   /* TODO */
-  return 0;
+  size_t len;
+  pthread_mutex_lock(&wclist->lock);
+  len = list_size(&wclist->lst);
+  pthread_mutex_unlock(&wclist->lock);
+  return len;
+}
+// Helper function without locking for internal use
+static word_count_t* find_word_internal(word_count_list_t* wclist, char* word) {
+    struct list_elem* e;
+    for (e = list_begin(&wclist->lst); e != list_end(&wclist->lst); e = list_next(e)) {
+        word_count_t* wc = list_entry(e, word_count_t, elem);
+        if (strcmp(wc->word, word) == 0) {
+            return wc;
+        }
+    }
+    return NULL;
 }
 
 word_count_t* find_word(word_count_list_t* wclist, char* word) {
   /* TODO */
-  return NULL;
+  // it is like word_count_l.c but with mutexes
+  pthread_mutex_lock(&wclist->lock);
+  word_count_t* wc = find_word_internal(wclist, word);
+  pthread_mutex_unlock(&wclist->lock);
+  return wc;
 }
 
 word_count_t* add_word(word_count_list_t* wclist, char* word) {
   /* TODO */
-  return NULL;
+  pthread_mutex_lock(&wclist->lock);
+  word_count_t *existing = find_word_internal(wclist, word);
+  if (existing != NULL) {
+    existing->count++;
+    pthread_mutex_unlock(&wclist->lock);
+    return existing;
+  }
+
+  // create new word_count_t
+  word_count_t *new_node = malloc(sizeof(word_count_t));
+  if (new_node == NULL) {
+    pthread_mutex_unlock(&wclist->lock);
+    return NULL;
+  }
+  
+  new_node->word = strdup(word);
+  if (new_node->word == NULL) {
+    free(new_node);
+    pthread_mutex_unlock(&wclist->lock);
+    return NULL;
+  }
+
+  new_node->count = 1;
+  list_push_back(&wclist->lst, &new_node->elem);
+  pthread_mutex_unlock(&wclist->lock);
+
+  return new_node;
+
 }
 
 void fprint_words(word_count_list_t* wclist, FILE* outfile) {
   /* TODO */
   /* Please follow this format: fprintf(<file>, "%i\t%s\n", <count>, <word>); */
+
+  pthread_mutex_lock(&wclist->lock);
+  struct list_elem *e;
+  for (e = list_begin(&wclist->lst); e != list_end(&wclist->lst); e = list_next(e)) {
+    word_count_t* wc = list_entry(e, word_count_t, elem);
+    fprintf(outfile, "%i\t%s\n", wc->count, wc->word);
+  }
+  pthread_mutex_unlock(&wclist->lock);
+
+}
+static bool less_list(const struct list_elem* ewc1, const struct list_elem* ewc2, void* aux) {
+  /* TODO */
+  word_count_t* wc1 = list_entry(ewc1, word_count_t, elem);
+  word_count_t* wc2 = list_entry(ewc2, word_count_t, elem);
+    // Cast aux to the correct function pointer type
+  bool (*compare)(const word_count_t*, const word_count_t*) = (bool (*)(const word_count_t*, const word_count_t*))aux;
+
+  // Use the passed-in comparator
+  return compare(wc1, wc2);
 }
 
 void wordcount_sort(word_count_list_t* wclist,
                     bool less(const word_count_t*, const word_count_t*)) {
   /* TODO */
+  pthread_mutex_lock(&wclist->lock);
+  list_sort(&wclist->lst, less_list, less);
+  pthread_mutex_unlock(&wclist->lock);
+
 }
