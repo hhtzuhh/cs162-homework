c83cdb2698695a736485db06f7dc809c6fe27457
tzuhan <hhtzuhh@gmail.com>
2025-04-14 20:13:35 +0000

    pipes implementation

--- hw-shell/shell.c
+++ hw-shell/shell.c
@@ -109,17 +109,44 @@ int cmd_pwd(unused struct tokens* tokens) {
 
 /* Changes current working directory */
 int cmd_cd(struct tokens* tokens) {
-  // length can only be 2? what if it's more than 2?
-  // and other cd functions
-    if (tokens_get_length(tokens) != 2) {
-        fprintf(stderr, "cd: missing directory argument\n");
-        return 1;  
+    if (tokens_get_length(tokens) > 2) {
+        fprintf(stderr, "cd: too many arguments\n");
+        return 1;
     }
-    // TODO:handle ~,and check some other cases, check online
-    const char* dir = tokens_get_token(tokens, 1);
+
+    const char* dir;
+    if (tokens_get_length(tokens) == 1) {
+        // If no argument, change to home directory
+        dir = getenv("HOME");
+        if (!dir) {
+            fprintf(stderr, "cd: HOME environment variable not set\n");
+            return 1;
+        }
+    } else {
+        dir = tokens_get_token(tokens, 1);
+        // Handle ~ expansion
+        if (dir[0] == '~') {
+            const char* home = getenv("HOME");
+            if (!home) {
+                fprintf(stderr, "cd: HOME environment variable not set\n");
+                return 1;
+            }
+            if (dir[1] == '\0') {
+                dir = home;
+            } else if (dir[1] == '/') {
+                char* expanded = malloc(strlen(home) + strlen(dir + 1) + 1);
+                if (!expanded) {
+                    perror("malloc");
+                    return 1;
+                }
+                sprintf(expanded, "%s%s", home, dir + 1);
+                dir = expanded;
+            }
+        }
+    }
     if (chdir(dir) != 0) {
         perror("cd");
-        return 1;  
+        return 1;
     }
     return 0;
 }
@@ -147,8 +174,6 @@ static char* resolve_program_path(const char* program) {
     if (!path_env) {
         return NULL;
     }
-    printf("path string: %s\n", path_env);
-
     // Make a copy of PATH for strtok_r
     char* path_copy = strdup(path_env);
     if (!path_copy) {
@@ -187,15 +212,197 @@ static char* resolve_program_path(const char* program) {
     return NULL;
 }
 
+/* Helper function to execute a single command segment */
+static int execute_command_segment(struct tokens* tokens, int start, int end, 
+                                 int pipe_in, int pipe_out) {
+    // Set up input/output redirection
+    if (pipe_in != -1) {
+        dup2(pipe_in, STDIN_FILENO);
+        close(pipe_in);
+    }
+    if (pipe_out != -1) {
+        dup2(pipe_out, STDOUT_FILENO);
+        close(pipe_out);
+    }
+
+    // Check if it's a built-in command
+    int fundex = lookup(tokens_get_token(tokens, start));
+    if (fundex >= 0) {
+        int status = cmd_table[fundex].fun(tokens);
+        exit(status);
+    }
+
+    // Handle program execution
+    const char* program = tokens_get_token(tokens, start);
+    char* full_path = resolve_program_path(program);
+    if (!full_path) {
+        fprintf(stderr, "%s: command not found\n", program);
+        exit(1);
+    }
+
+    // Count redirection operators and build argv
+    int count = 0;
+    int in_fd = -1, out_fd = -1;
+    for (int i = start; i < end; i++) {
+        char* token = tokens_get_token(tokens, i);
+        if (strcmp(token, "<") == 0) {
+            ++i;
+            if ((in_fd = open(tokens_get_token(tokens, i), O_RDONLY)) < 0) {
+                perror("open input file");
+                free(full_path);
+                exit(1);
+            }
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
+            count++;
+            continue;
+        }
+        if (strcmp(token, ">") == 0) {
+            ++i;
+            out_fd = open(tokens_get_token(tokens, i), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (out_fd < 0) {
+                perror("open output file");
+                free(full_path);
+                exit(1);
+            }
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
+            count++;
+            continue;
+        }
+    }
+
+    // Build argv array
+    int argc = end - start - (count * 2);
+    char* argv[argc + 1];
+    argv[0] = (char*)full_path;
+
+    int arg_index = 1;
+    for (int i = start + 1; i < end; i++) {
+        char* token = tokens_get_token(tokens, i);
+        if (!strcmp(token, "<") || !strcmp(token, ">")) {
+            i++;
+            continue;
+        }
+        argv[arg_index++] = token;
+    }
+    argv[arg_index] = NULL;
+
+    execv(full_path, argv);
+    perror("execv");
+    free(full_path);
+    exit(1);
+}
+
+/* Helper function to execute a pipeline of commands */
+static int execute_pipeline(struct tokens* tokens) {
+
+    // Count number of commands (number of pipes + 1)
+    int num_commands = 1;
+    for (int i = 0; i < tokens_get_length(tokens); i++) {
+        if (strcmp(tokens_get_token(tokens, i), "|") == 0) {
+            num_commands++;
+        }
+    }
+    // Check if it's a built-in command (no pipes)
+    if (num_commands == 1) {
+        int fundex = lookup(tokens_get_token(tokens, 0));
+        if (fundex >= 0) {
+            int status = cmd_table[fundex].fun(tokens);
+            // If it's exit command, exit the entire shell
+            if (strcmp(cmd_table[fundex].cmd, "exit") == 0) {
+                exit(0);
+            }
+            return status;
+        }
+    }
+
+
+
+    // Create pipes
+    int (*pipes)[2] = malloc((num_commands - 1) * sizeof(int[2]));
+    if (!pipes) {
+        perror("malloc");
+        return -1;
+    }
+
+    for (int i = 0; i < num_commands - 1; i++) {
+        if (pipe(pipes[i]) < 0) {
+            perror("pipe");
+            free(pipes);
+            return -1;
+        }
+    }
+
+    // Fork and execute commands
+    int start = 0;
+    int segment = 0;
+    pid_t* pids = malloc(num_commands * sizeof(pid_t));
+    if (!pids) {
+        perror("malloc");
+        free(pipes);
+        return -1;
+    }
+
+    for (int i = 0; i <= tokens_get_length(tokens); i++) {
+        if (i == tokens_get_length(tokens) || 
+            strcmp(tokens_get_token(tokens, i), "|") == 0) {
+            pids[segment] = fork();
+            if (pids[segment] == 0) {
+                // Child process
+                int pipe_in = (segment > 0) ? pipes[segment-1][0] : -1;
+                int pipe_out = (segment < num_commands-1) ? pipes[segment][1] : -1;
+                
+                // Close pipe ends in child, only preserve the needed one in this child process
+                for (int j = 0; j < num_commands - 1; j++) {
+                    if (j != segment - 1) close(pipes[j][0]); // close read end
+                    if (j != segment) close(pipes[j][1]); // close write end
+                }
+
+                execute_command_segment(tokens, start, i, pipe_in, pipe_out);
+            } else if (pids[segment] < 0) {
+                perror("fork");
+                free(pipes);
+                free(pids);
+                return -1;
+            }
+            start = i + 1;
+            segment++;
+        }
+    }
+
+    // Parent process: close all pipe ends
+    for (int i = 0; i < num_commands - 1; i++) {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
+    // Wait for all children
+    for (int i = 0; i < num_commands; i++) {
+        int status;
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            if (exit_status != 0) {
+                fprintf(stderr, "Process %d exited with status %d\n", i, exit_status);
+            }
+        }
+    }
+
+    free(pipes);
+    free(pids);
+    return 0;
+}
+
 int main(unused int argc, unused char* argv[]) {
   init_shell();
 
   static char line[4096];
-  int line_num = 0;
+//   int line_num = 0;
 
   /* Please only print shell prompts when standard input is not a tty */
-  if (!shell_is_interactive)
-    fprintf(stdout, "%d: ", line_num);
+//   if (!shell_is_interactive) 
+//     fprintf(stdout, "%d: ", line_num);
 
   while (fgets(line, 4096, stdin)) {
     /* Split our line into words. */
@@ -208,116 +415,16 @@ int main(unused int argc, unused char* argv[]) {
     }
 
     // I want to print all the tokens
-    for (int i = 0; i < tokens_get_length(tokens); i++) {
-      printf("token %d: %s\n", i, tokens_get_token(tokens, i));
-    }
+    // for (int i = 0; i < tokens_get_length(tokens); i++) {
+    //   printf("token %d: %s\n", i, tokens_get_token(tokens, i));
+    // }
 
-    /* Find which built-in function to run. */
-    int fundex = lookup(tokens_get_token(tokens, 0));
+    /* Execute program or pipeline */
+    execute_pipeline(tokens);
 
-    if (fundex >= 0) {
-      cmd_table[fundex].fun(tokens);
-    } else {
-      /* Execute program */
-      const char* program = tokens_get_token(tokens, 0);
-      char* full_path = resolve_program_path(program);
-      
-      if (!full_path) {
-        fprintf(stderr, "%s: command not found\n", program);
-        tokens_destroy(tokens);
-        continue;
-      }
-      //for loop the tokens_get_token from index 1
-      // and check if contain <, and >. and chec
-      pid_t pid = fork();
-      if (pid == 0) {
-        // Child process
-        char *token_per;
-        int count = 0;
-        int in = -1, out = -1;
-        // define in and out
-        for (int i = 0; i < tokens_get_length(tokens); i++) {
-          token_per = tokens_get_token(tokens, i);
-          if (strcmp(token_per, "<") == 0) {
-            ++i;
-            if ((in = open(tokens_get_token(tokens, i), O_RDONLY)) < 0) {
-              perror("open input file");
-              free(full_path);
-              exit(1);
-            }
-            dup2(in, STDIN_FILENO);         // duplicate stdin to input file
-            close(in);                      // close after use
-            count++;
-            continue;
-          }
-
-          if (strcmp(token_per, ">") == 0) {
-            ++i;
-            out = open(tokens_get_token(tokens, i), O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (out < 0) {
-              perror("open output file");
-              free(full_path);
-              exit(1);
-            }
-            dup2(out, STDOUT_FILENO);       // redirect stdout to file
-            close(out);                     // close after use
-            count++;
-            continue;
-          }
-        }
-
-        count = count*2; // operator , ">", and the file should not in args
-        // Convert tokens to argv array
-        int argc = tokens_get_length(tokens);
-        argc = argc - count;
-        char* argv[argc + 1];  // +1 for NULL terminator
-        
-        // First argument is the full path
-        argv[0] = full_path;
-
-
-
-        // Copy remaining arguments, and skip
-        int arg_index = 1;
-        for (int i = 1; i < argc; i++) {
-          char *tok = tokens_get_token(tokens, i);
-          if (!strcmp(tok, "<") || !strcmp(tok, ">")) {
-            i++;
-            continue;
-          }
-          argv[arg_index++] = tok;
-        }
-        argv[arg_index] = NULL;  // NULL terminate the array
-        
-        // Execute the program
-        execv(full_path, argv);
-        
-        // If we get here, execv failed
-        perror("execv");
-        free(full_path);
-        exit(1);
-      } else if (pid > 0) {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
-        
-        if (WIFEXITED(status)) {
-          int exit_status = WEXITSTATUS(status);
-          if (exit_status != 0) {
-            fprintf(stderr, "Program exited with status %d\n", exit_status);
-          }
-        }
-        free(full_path);
-      } else {
-        // Fork failed
-        perror("fork");
-        free(full_path);
-      }
-    }
-
-    if (!shell_is_interactive)
-      /* Please only print shell prompts when standard input is not a tty */
-      fprintf(stdout, "%d: ", ++line_num);
+    // if (!shell_is_interactive)
+    //   /* Please only print shell prompts when standard input is not a tty */
+    //   fprintf(stdout, "%d: ", ++line_num);
 
     /* Clean up memory */
     tokens_destroy(tokens);
